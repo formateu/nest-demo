93c1edc8e5eaa51708b58c34e6a70f6ec22a885d
Mateusz Forc <formateu@gmail.com>
2021-10-18 14:48:24 +0200

    Add unit tests

---
+++ src/currency-calculator/currency-calculator.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CurrencyCalculatorController } from './currency-calculator.controller';
+import { CurrencyCalculatorService } from './currency-calculator.service';
+import { of } from 'rxjs';
+import { HttpModule } from '@nestjs/axios';
+
+describe('CurrencyCalculatorController', () => {
+  let controller: CurrencyCalculatorController;
+  let ccservice = CurrencyCalculatorService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [CurrencyCalculatorController],
+      providers: [CurrencyCalculatorService],
+      imports: [HttpModule],
+    }).compile();
+
+    controller = app.get<CurrencyCalculatorController>(
+      CurrencyCalculatorController,
+    );
+  });
+
+  describe('currencyCalculator/currency', () => {
+    it('Should return current PLN to given currency rate"', () => {
+      const mockCurrency = 'usd';
+      const result = of(3);
+      jest
+        .spyOn(ccservice, 'getCurrencyRate')
+        .mockImplementation((_: string) => result);
+      expect(controller.getCurrencyRate(mockCurrency)).toBe(result);
+    });
+  });
+});
